f2a7c419a25caa6ed16a2216db80b33cf5b1f229
camilleNicole <camilleht@gmail.com>
2022-03-26 21:14:18 -0400

    routes

--- models/Post.js
+++ models/Post.js
@@ -17,7 +17,7 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    post_text: {
+    text: {
       type: DataTypes.TEXT,
       allowNull: false
     },
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,7 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
-  const text = document.querySelector('input[text="post-text"]').value.trim();
+  const text = document.querySelector('input[name="post-text"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
@@ -10,7 +10,7 @@ async function editFormHandler(event) {
     method: 'PUT',
     body: JSON.stringify({
       title,
-      text,
+      text
     }),
     headers: {
       'Content-Type': 'application/json'
