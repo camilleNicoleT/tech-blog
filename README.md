aa0dada1c7e9fdf1491a4c164edec7ca426fb8b5
camilleNicoleT <94029792+camilleNicoleT@users.noreply.github.com>
2022-03-27 22:47:09 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# tech-blog
+Tech-blog
+Github Link: https://github.com/camilleNicoleT/tech-blog.git
+
+## Description
+The User can read, post and update blog posts!
+
+## Table of Contents:
+   - [Usage](#usage) 
+   - [Questions](#Questions) 
+
+## Usage
+  Receive data of retail product items and tracking their price, how much in stock with relation to category and tag ids.
+
+
+## Questions
+For comments or questions contact me through GitHub. Username: https://github.com/camilleNicoleT
---
+++ seeds/post-seeds.js
@@ -0,0 +1,89 @@
+const { Post } = require('../models');
+
+const postdata = [
+  {
+    title: 'Donec posuere metus vitae ipsum.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 10
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 8
+  },
+  {
+    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 1
+  },
+  {
+    title: 'Nunc purus.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 4
+  },
+  {
+    title: 'Pellentesque eget nunc.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 7
+  },
+  {
+    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 4
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 1
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 1
+  },
+  {
+    title: 'Duis ac nibh.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 9
+  },
+  {
+    title: 'Curabitur at ipsum ac tellus semper interdum.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 5
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 3
+  },
+  {
+    title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 10
+  },
+  {
+    title: 'Donec dapibus.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 8
+  },
+  {
+    title: 'Nulla tellus.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 3
+  },
+  {
+    title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
+    text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 3
+  },
+  {
+    title:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
+      text: 'Donec posuere metus vitae ipsum. Donec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsumDonec posuere metus vitae ipsum',
+    user_id: 7
+  }
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
