cfabbcb1323875e7625edfa7fccc3224a9ebb9cf
camilleNicole <camilleht@gmail.com>
2022-03-27 21:20:19 -0400

    final styling

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -78,7 +78,7 @@ router.post('/', withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
-    text: req.body.title,
+    text: req.body.text,
     post_url: req.body.post_url,
     user_id: req.session.user_id
   })
